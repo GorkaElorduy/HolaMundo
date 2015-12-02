fcc417698b438f61e8933fc565a0579a2e80f11d
Curso <Curso@GBRB3.ipartekaula.com>
2015-12-02 09:14:13 +0100

    cambio tonto

--- src/java/com/ipartek/formacion/holamundo/pruebas/ParImpar.java
+++ src/java/com/ipartek/formacion/holamundo/pruebas/ParImpar.java
@@ -6,7 +6,7 @@ public class ParImpar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//CAMBIO TONTO
 		System.out.println("Primer ejercicio: ");
 
 		for (int i = 1; i <= 10; i++) {
